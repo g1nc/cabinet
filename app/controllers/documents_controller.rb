f88835eb14eeac2892c8eb6067e68e403033e95a
g1nc <grimm.hellg@gmail.com>
2016-09-21 23:43:34 +0300

    Association between users and documents

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -9,11 +9,11 @@ class DocumentsController < ApplicationController
 	end
 
 	def new
-		@document = Document.new
+		@document = current_user.docs.build
 	end
 
 	def create
-		@document = Document.new(document_params)
+		@document = current_user.docs.build(document_params)
 
 		if @document.save
 			redirect_to @document
